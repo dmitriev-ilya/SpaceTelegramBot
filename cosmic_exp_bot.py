609bf6818f0ef3ef37f2ae81d72d4cd4d01b263f
Ilusha <67222917+dmitriev-ilya@users.noreply.github.com>
2022-11-12 20:15:40 +0500

    publish text to Telegram channel.

---
+++ cosmic_exp_bot.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import telegram
+import os
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
+    chat_id = '@cosmo_odisey'
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    print(bot.get_me())
+
+    bot.send_message(chat_id=chat_id, text="Hello World!")
+
