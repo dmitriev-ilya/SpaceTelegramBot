82f786b21a2a63e7ca6ceb3b4721ec7846f29c7e
Ilusha <67222917+dmitriev-ilya@users.noreply.github.com>
2022-11-13 11:54:06 +0500

    Бот выделен в самостоятельный скрипт

---
+++ image_bot.py
@@ -0,0 +1,37 @@
+from dotenv import load_dotenv
+import telegram
+import os
+import argparse
+
+
+def send_photo_to_chat(image_path, chat_id, bot_token):
+	bot = telegram.Bot(token=bot_token)
+	bot.send_photo(chat_id=chat_id, photo=open(image_path, 'rb'))
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    bot_token = os.environ['TELEGRAM_TOKEN']
+    chat_id = '@cosmo_odisey'
+    image_path = 'images/epic_nasa_0.jpeg'
+
+    parser = argparse.ArgumentParser(
+        description="""Бот загружает фотографии в указанный Telegram Chat.
+        Бот должен быть админом чата.
+        Требует получения TELEGRAM_TOKEN, который должен быть указан в переменной .env"""
+    )
+    parser.add_argument(
+        '-i',
+        '--image_path', 
+        help='путь к картинке',
+    )
+    parser.add_argument(
+    '-id',
+    '--chat_id', 
+    help='ссылка на чат, в формате @my_tel_ch',
+    )
+    args = parser.parse_args()
+    
+    send_photo_to_chat(args.image_path, args.chat_id, bot_token)
+    
+
