85859939a0953d1021758f0302cba9a27bdfaa97
Ilusha <67222917+dmitriev-ilya@users.noreply.github.com>
2022-11-20 11:35:18 +0500

    Код сделан более плоским

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -14,15 +14,13 @@ def fetch_spacex_launch(launch_id):
     response = requests.get(f"https://api.spacexdata.com/v5/launches/{launch_id}")
     response.raise_for_status()
     images_set = response.json()["links"]["flickr"]["original"]
-    if images_set:
-        for number, image_url in enumerate(images_set, start=1):
-            filename = os.path.join("images", f"spacex_{number}.jpeg")
-            main_loader_functions.load_picture(image_url, filename)
-            print(f"Photo {number} is load")
-
-        print("It's done!")
-    else:
+    if not images_set:
         raise EmptyImagesSetError("No images in this launch")
+    for number, image_url in enumerate(images_set, start=1):
+        filename = os.path.join("images", f"spacex_{number}.jpeg")
+        main_loader_functions.load_picture(image_url, filename)
+        print(f"Photo {number} is load")
+    print("It's done!")
 
 
 if __name__ == "__main__":
